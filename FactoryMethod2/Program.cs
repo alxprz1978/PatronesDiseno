444b2692e6850ff710b8c0d04e2f0696e2e533a7
alxprz1978 <alx.prz@gmail.com>
2022-02-07 23:15:13 -0600

    Agregar archivos de proyecto.

---
+++ FactoryMethod2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod2
+{
+    public abstract class CombustibleVehiculo
+    {
+        public abstract int CuantoOctanaje();
+
+        public string OperacionComun()
+        {
+            return "El octanaje de este combistible es:";
+        }
+    }
+    
+    public class GasolinaMagna : CombustibleVehiculo
+    {
+        public override int CuantoOctanaje()
+        {
+            return 87;
+        }
+    }
+
+    public class GasolinaPremium : CombustibleVehiculo
+    {
+        public override int CuantoOctanaje()
+        {
+            return 92;
+        }
+    }
+
+    public class Vehiculo
+    {
+        public const int MAGNA = 1;
+        public const int PREMIUM = 2;
+
+        public static CombustibleVehiculo VehiculoUsa(int tipo)
+        {
+            switch(tipo)
+            {
+                case MAGNA:
+                    return new GasolinaMagna();
+                case PREMIUM:
+                    return new GasolinaPremium();
+                default:
+                    return null;
+            }
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CombustibleVehiculo oCombustible = Vehiculo.VehiculoUsa(Vehiculo.MAGNA);
+            Console.WriteLine(oCombustible.OperacionComun());
+            Console.WriteLine(oCombustible.CuantoOctanaje());
+
+            CombustibleVehiculo oCombustible2 = Vehiculo.VehiculoUsa(Vehiculo.PREMIUM);
+            Console.WriteLine(oCombustible2.OperacionComun());
+            Console.WriteLine(oCombustible2.CuantoOctanaje());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PatronesDiseño/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronesDiseño
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
